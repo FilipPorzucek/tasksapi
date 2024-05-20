4e0cec5b7c04a8f1830e4e3eaef83c82fbad3299
FilipPorzucek <147589167+FilipPorzucek@users.noreply.github.com>
2024-05-20 14:09:23 +0200

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,31 +1,67 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
 
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
- const data = fs.readFileSync(filePath, "utf-8");
- const db = JSON.parse(data);
-const router = jsonServer.router(db)
+// Ścieżka do pliku db.json (zakładając, że jest poza folderem api)
+const dbFilePath = path.join(__dirname, '..', 'db.json');
 
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
+// Wczytaj dane z pliku db.json
+const dbData = fs.readFileSync(dbFilePath, 'utf8');
+const db = JSON.parse(dbData);
 
-const middlewares = jsonServer.defaults()
+// Utwórz router JSON Server z danymi z pliku db.json
+const router = jsonServer.router(db);
 
-server.use(middlewares)
-// Add this before server.use(router)
+server.use(middlewares);
+
+// Dodaj rewriter middleware
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+}));
 
-// Export the Server API
-module.exports = server
+// Obsługa dodawania nowych danych
+server.post('/api/tasks', (req, res, next) => {
+    const newData = req.body;
+    db.tasks.push(newData);
+    saveDataToDB();
+    res.jsonp(newData);
+});
+
+// Obsługa usuwania danych
+server.delete('/api/tasks/:id', (req, res, next) => {
+    const taskId = parseInt(req.params.id);
+    db.tasks = db.tasks.filter(task => task.id !== taskId);
+    saveDataToDB();
+    res.jsonp({ success: true });
+});
+
+// Obsługa aktualizowania danych
+server.patch('/api/tasks/:id', (req, res, next) => {
+    const taskId = parseInt(req.params.id);
+    const updatedData = req.body;
+    db.tasks = db.tasks.map(task => (task.id === taskId ? { ...task, ...updatedData } : task));
+    saveDataToDB();
+    res.jsonp(updatedData);
+});
+
+// Funkcja do zapisywania danych do pliku db.json
+function saveDataToDB() {
+    fs.writeFile(dbFilePath, JSON.stringify(db, null, 2), 'utf8', err => {
+        if (err) {
+            console.error('Error writing db.json:', err);
+        } else {
+            console.log('Data has been written to db.json');
+        }
+    });
+}
+
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+});
