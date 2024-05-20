8bc87416b9bb328ac41c61fd3f694e0429e27869
FilipPorzucek <147589167+FilipPorzucek@users.noreply.github.com>
2024-05-20 13:41:35 +0200

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,67 +1,31 @@
-const jsonServer = require('json-server');
-const fs = require('fs');
-const path = require('path');
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-const server = jsonServer.create();
-const middlewares = jsonServer.defaults();
+const server = jsonServer.create()
 
-server.use(middlewares);
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+ const data = fs.readFileSync(filePath, "utf-8");
+ const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
-const router = jsonServer.router('db.json'); // Używamy pliku db.json bezpośrednio
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}));
-server.use(router);
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-// Wczytujemy dane z pliku db.json tylko raz, przy uruchomieniu serwera
-fs.readFile('db.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading db.json:', err);
-        return;
-    }
-    router.db.setState(JSON.parse(data));
-    console.log('Data has been read from db.json');
-});
-
-// Funkcja do zapisywania danych do pliku db.json
-function saveDataToDB() {
-    fs.writeFile('db.json', JSON.stringify(router.db.getState()), 'utf8', (err) => {
-        if (err) {
-            console.error('Error writing db.json:', err);
-        } else {
-            console.log('Data has been written to db.json');
-        }
-    });
-}
-
-// Obsługa dodawania nowych danych
-server.post('/api/tasks', (req, res) => {
-    const newData = req.body;
-    router.db.get('tasks').push(newData).write();
-    saveDataToDB();
-    res.jsonp(newData);
-});
-
-// Obsługa usuwania danych
-server.delete('/api/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id);
-    router.db.get('tasks').remove({ id: taskId }).write();
-    saveDataToDB();
-    res.jsonp({ success: true });
-});
-
-// Obsługa aktualizowania danych
-server.patch('/api/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id);
-    const updatedData = req.body;
-    router.db.get('tasks').find({ id: taskId }).assign(updatedData).write();
-    saveDataToDB();
-    res.jsonp(updatedData);
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`);
-});
+// Export the Server API
+module.exports = server
