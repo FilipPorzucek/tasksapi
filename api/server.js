3c361fa2149462385787959192bdbc984c19743d
FilipPorzucek <147589167+FilipPorzucek@users.noreply.github.com>
2024-05-20 22:17:07 +0200

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -5,8 +5,24 @@ const path = require('path');
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
+// Ścieżka do pliku db.json (zakładając, że jest poza folderem api)
+const dbFilePath = path.join(__dirname, '..', 'db.json');
+
+// Wczytaj dane z pliku db.json
+let db = {};
+
+try {
+    const dbData = fs.readFileSync(dbFilePath, 'utf8');
+    db = JSON.parse(dbData);
+} catch (error) {
+    console.error('Error reading db.json:', error);
+}
+
 // Utwórz router JSON Server z danymi z pliku db.json
-let router;
+const router = jsonServer.router(db);
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
 // Dodaj rewriter middleware
 server.use(jsonServer.rewriter({
@@ -16,35 +32,31 @@ server.use(jsonServer.rewriter({
 
 // Obsługa dodawania nowych danych
 server.post('/api/tasks', (req, res, next) => {
-    const db = loadDB(); // Wczytaj dane z pliku db.json
     const newData = req.body;
     db.tasks.push(newData);
-    saveDataToDB(db);
+    saveDataToDB();
     res.jsonp(newData);
 });
 
 // Obsługa usuwania danych
 server.delete('/api/tasks/:id', (req, res, next) => {
-    const db = loadDB(); // Wczytaj dane z pliku db.json
     const taskId = parseInt(req.params.id);
     db.tasks = db.tasks.filter(task => task.id !== taskId);
-    saveDataToDB(db);
+    saveDataToDB();
     res.jsonp({ success: true });
 });
 
 // Obsługa aktualizowania danych
 server.patch('/api/tasks/:id', (req, res, next) => {
-    const db = loadDB(); // Wczytaj dane z pliku db.json
     const taskId = parseInt(req.params.id);
     const updatedData = req.body;
     db.tasks = db.tasks.map(task => (task.id === taskId ? { ...task, ...updatedData } : task));
-    saveDataToDB(db);
+    saveDataToDB();
     res.jsonp(updatedData);
 });
 
 // Funkcja do zapisywania danych do pliku db.json
-function saveDataToDB(db) {
-    const dbFilePath = path.join(__dirname, '..', 'db.json');
+function saveDataToDB() {
     fs.writeFile(dbFilePath, JSON.stringify(db, null, 2), 'utf8', err => {
         if (err) {
             console.error('Error writing db.json:', err);
@@ -52,13 +64,6 @@ function saveDataToDB(db) {
             console.log('Data has been written to db.json');
         }
     });
-}
-
-// Funkcja do wczytywania danych z pliku db.json
-function loadDB() {
-    const dbFilePath = path.join(__dirname, '..', 'db.json');
-    const dbData = fs.readFileSync(dbFilePath, 'utf8');
-    return JSON.parse(dbData);
 }
 
 server.use(router);
