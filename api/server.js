d6ee605bbd6d003c3fa1fa496cc42cf168475a5d
FilipPorzucek <147589167+FilipPorzucek@users.noreply.github.com>
2024-05-20 11:03:01 +0200

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,27 +1,25 @@
 const jsonServer = require('json-server');
 const path = require('path');
-const fs = require('fs');
+
 const server = jsonServer.create();
-const filePath = path.join(__dirname, 'db.json');
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // Używaj pliku db.json bezpośrednio
 
-if (!fs.existsSync(filePath)) {
-  console.error('db.json file not found');
-  process.exit(1);
-}
-
-const router = jsonServer.router(filePath);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+// Dodaj przepisanie URL
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/blog/:resource/:id/show': '/:resource/:id'
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
 }));
+
 server.use(router);
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+    console.log('JSON Server is running on port', port);
 });
 
+// Export the Server API
 module.exports = server;
