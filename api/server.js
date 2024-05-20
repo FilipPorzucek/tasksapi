62e0b2ca44a739935e44fe1170aa08b4ef1c6798
FilipPorzucek <147589167+FilipPorzucek@users.noreply.github.com>
2024-05-20 11:53:48 +0200

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -16,28 +16,24 @@ server.use(jsonServer.rewriter({
 server.use(router);
 
 // Wczytujemy dane z pliku db.json tylko raz, przy uruchomieniu serwera
-let dataRead = false;
 fs.readFile('db.json', 'utf8', (err, data) => {
     if (err) {
         console.error('Error reading db.json:', err);
         return;
     }
     router.db.setState(JSON.parse(data));
-    dataRead = true;
     console.log('Data has been read from db.json');
 });
 
 // Obsługujemy zapisywanie zmian do pliku db.json w przypadku zmian w danych
 router.render = (req, res) => {
-    if (dataRead) {
-        fs.writeFile('db.json', JSON.stringify(router.db.getState()), 'utf8', (err) => {
-            if (err) {
-                console.error('Error writing db.json:', err);
-            } else {
-                console.log('Data has been written to db.json');
-            }
-        });
-    }
+    fs.writeFile('db.json', JSON.stringify(router.db.getState()), 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing db.json:', err);
+        } else {
+            console.log('Data has been written to db.json');
+        }
+    });
     res.jsonp(res.locals.data);
 };
 
